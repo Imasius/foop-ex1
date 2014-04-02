e3825cb7e0398f7f398190ce28fbefbe17b6dc73
Markus Scherer <markus.f.scherer@gmail.com>
2014-04-02 10:45:38 +0200

    Added Interface for representing a Level.

---
+++ src/main/java/mouse/shared/Level.java
@@ -0,0 +1,19 @@
+package mouse.shared;
+
+import java.awt.Point;
+import java.util.Collection;
+
+/**
+ * Abstract Level to be used on both Server and Client.
+ * Coordinates start at (0,0) in the upper-left corner and range to 
+ * (width-1, height-1).
+ * User: Markus
+ * Date: 02.04.14
+ */
+public interface Level {
+	public int getHeight();
+	public int getWidth();
+	public Tile tileAt(int x, int y);
+	public Point getBaitPosition();
+	public Collection<Point> getStartpositions();
+}
---
+++ src/main/java/mouse/shared/Tile.java
@@ -0,0 +1,10 @@
+package mouse.shared;
+
+/**
+ * Describes the types of tiles form a level.
+ * User: Markus
+ * Date: 02.04.14
+ */
+public enum Tile {
+	EMPTY, WALL, DOOR_OPEN, DOOR_CLOSED
+}
